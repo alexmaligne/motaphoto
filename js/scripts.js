1abe60d3b7f2de7f5f8bc4689bd4306c5a3f11f6
Alexandra Maligne <alexandra.maligne@gmail.com>
2023-07-25 18:59:21 +0200

    modale de contact

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,9 @@ let contactLink = document.querySelector("#menu-item-68 a")
 contactLink.addEventListener("click", function (){
     contactForm.style.display = "block"
 })
+
+window.onclick = function(event) {
+    if (event.target == contactForm) {
+        contactForm.style.display = "none";
+    }
+}
